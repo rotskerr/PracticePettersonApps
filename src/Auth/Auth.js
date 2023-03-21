bf2a04717ab1ba257768066aebec5e89f8ae2379
rotskerr <vvribakk@gmail.com>
2023-03-21 13:15:06 +0200

    Login/Sigup Page

---
+++ src/Auth/Auth.js
@@ -0,0 +1,25 @@
+import { Button, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+import './Auth.css'
+import { useState, inputs } from 'react'
+
+const Auth = () => {
+    const [isSignup, setisSignup] = useState(false);
+    return (
+        <div>
+            <form>
+                <Box className='LoginBox'>
+                    <Typography variant='h4' padding={3}>{isSignup ? "Signup" : "Login"}</Typography>
+                    <TextField  type={'text'} variant='outlined' placeholder='name' margin='normal' />
+                    {isSignup && <TextField  type={'email'} variant='outlined' placeholder='email' margin='normal' />}
+                    <TextField  type={'password'} variant='outlined' placeholder='password' margin='normal' />
+                    <Button sx={{ marginTop: 3 }} variant='contained' color='success' >{isSignup ? "Signup" : "Login"}</Button>
+                    <Button onClick={() => setisSignup(!isSignup)} sx={{ marginTop: 3 }} >Change To {isSignup ? "Login" : "Signup"}</Button>
+                </Box>
+            </form>
+        </div>
+    )
+}
+
+export default Auth
