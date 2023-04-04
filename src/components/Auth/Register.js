f40d93e676fb45420595378093c810bcaf1c9857
rotskerr <vvribakk@gmail.com>
2023-04-04 20:49:46 +0300

    bug fixing

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,11 @@
-import { Button } from "@mui/material";
-import React, { useState } from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import React from "react";
+import "./Auth.css";
+import { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -19,7 +22,6 @@ const Login = () => {
 
 
   const handleApi = () => {
-    console.log({ email, password });
     axios
       .post(
         "https://practicepetersonapps.herokuapp.com/api/register",
@@ -37,6 +39,7 @@ const Login = () => {
       )
       .then((result) => {
         console.log(result);
+        localStorage.setItem('token', result.data.token);
       })
       .catch((error) => {
         console.log(error);
@@ -44,15 +47,61 @@ const Login = () => {
   };
   return (
     <div>   
-      name: <input value={name} onChange={handleName} type="text" />
+      {/* name: <input value={name} onChange={handleName} type="text" />
       <br />
       email: <input value={email} onChange={handleEmail} type="text" />
       <br />
       password: <input value={password} onChange={handlePassword} type="text" />
       <br />
-      <Button onClick={handleApi}>Login</Button>
+      <Button onClick={handleApi}>Login</Button> */}
+
+
+      <form className="LoginBox">
+        <Typography variant="h4" padding={3}>
+          Signup
+        </Typography>
+
+        <TextField
+          value={name}
+          onChange={handleName}
+          sx={{ input: { color: "#B8B7B7" } }}
+          type={"text"}
+          variant="outlined"
+          placeholder="email"
+          margin="normal"
+        />
+
+        <TextField
+          value={email}
+          onChange={handleEmail}
+          sx={{ input: { color: "#B8B7B7" } }}
+          type={"text"}
+          variant="outlined"
+          placeholder="email"
+          margin="normal"
+        />
+        <TextField
+          value={password}
+          onChange={handlePassword}
+          sx={{ input: { color: "#B8B7B7" } }}
+          type={"password"}
+          variant="outlined"
+          placeholder="password"
+          margin="normal"
+        />
+        <Button
+          onClick={handleApi}
+          sx={{ marginTop: 3 }}
+          variant="contained"
+          color="success"
+        >
+          Signup
+        </Button>
+        <Button sx={{ marginTop: 3, color: "#B8B7B7" }}><Link to="/login">Login</Link></Button>
+      </form>
+
     </div>
   );
 };
 
-export default Login;
+export default Register;
